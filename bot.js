ca2ceaf0fd858ce4e25f4a1fea6ba081ca94499e
Fast6 <45312443+Fast6@users.noreply.github.com>
2018-12-29 13:17:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,5 +40,13 @@ message.channel.sendEmbed(embed)
  
 }
 });
+client.on('message',function(message) {
+    let prefix = "$";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
 
 client.login(process.env.BOT_TOKEN);
